6deb0afb178004577415c4ed79565a504b262aa5
Nick Panin <panin@adwz.ru>
2020-01-19 02:27:31 +0300

    122 Task done
    - new screen finished

--- app/src/main/java/com/example/androidpanin/LessonTwoTaskOneActivity.java
+++ app/src/main/java/com/example/androidpanin/LessonTwoTaskOneActivity.java
@@ -2,7 +2,12 @@ package com.example.androidpanin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LessonTwoTaskOneActivity extends AppCompatActivity {
 
@@ -10,5 +15,48 @@ public class LessonTwoTaskOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_two_task_one);
+
+        final TextView formMsg = findViewById(R.id.formMsg);
+        final TextView formErr = findViewById(R.id.formErr);
+
+        final EditText nameInput = findViewById(R.id.nameInput);
+        nameInput.setText("");
+        final EditText emailInput = findViewById(R.id.emailInput);
+        emailInput.setText("");
+
+
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String name = nameInput.getText().toString();
+                final String email = emailInput.getText().toString();
+
+                final String formMsgText = "Подписка на рассылку успешно оформлена для пользователя " + name + " на электронный адрес " + email;
+                final String errText = "Не заполнено одно из полей!";
+
+                if ("".equals(name) || "".equals(email)) {
+                    formErr.setText(errText);
+                } else {
+                    formErr.setText("");
+                    formMsg.setText(formMsgText);
+                }
+            }
+        });
+
+        Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formErr.setText("");
+                formMsg.setText("");
+                nameInput.setText("");
+                emailInput.setText("");
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/androidpanin/LessonTwoTaskTwoActivity.java
+++ app/src/main/java/com/example/androidpanin/LessonTwoTaskTwoActivity.java
@@ -2,7 +2,13 @@ package com.example.androidpanin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class LessonTwoTaskTwoActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class LessonTwoTaskTwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_two_task_two);
+
+        TextView currentImageLink = findViewById(R.id.currentImageLink);
+        currentImageLink.setText(createLink());
+
+
+        Button forwardButton = findViewById(R.id.forwardButton);
+        forwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LessonTwoTaskTwoActivity.this, LessonTwoTaskTwoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button backwardButton = findViewById(R.id.backwardButton);
+        backwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LessonTwoTaskTwoActivity.this.finish();
+            }
+        });
+    }
+
+    public String createLink() {
+        return "http://myfile.org/" + new Random().nextInt(100);
     }
 }
