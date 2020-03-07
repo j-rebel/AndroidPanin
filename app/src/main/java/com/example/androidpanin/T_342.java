5803ade0718cd1186eca350f66f171457633cd6f
Nick Panin <panin@adwz.ru>
2020-03-07 23:48:07 +0300

    342 done

--- app/src/main/java/com/example/androidpanin/T_342.java
+++ app/src/main/java/com/example/androidpanin/T_342.java
@@ -3,21 +3,91 @@ package com.example.androidpanin;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class T_342 extends AppCompatActivity {
+import java.util.Locale;
+
+public class T_342 extends ToolbarActivity implements View.OnClickListener {
+
+    private Button mOkBtn;
+    private Spinner mLangSpinner;
+    private Spinner mMarginsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetMargins(this);
         setContentView(R.layout.activity_342);
         initViews();
+    }
 
+    @Override
+    public void onClick(View v) {
+
+        switch (mLangSpinner.getSelectedItemPosition()) {
+            case 0:
+                setLocaleAndColor("ru");
+                break;
+            case 1:
+                setLocaleAndColor("en");
+                break;
+        }
     }
 
     private void initViews() {
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        initLangSpinner();
+        initMarginSpinner();
+
+        mOkBtn = findViewById(R.id.okBtn);
+        mOkBtn.setOnClickListener(this);
+
     }
+
+    private void initLangSpinner() {
+
+        mLangSpinner = findViewById(R.id.langSpinner);
+
+        ArrayAdapter<CharSequence> adapterLang = ArrayAdapter.createFromResource(
+                this,
+                R.array.languages,
+                android.R.layout.simple_spinner_item
+        );
+        adapterLang.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mLangSpinner.setAdapter(adapterLang);
+
+    }
+
+    private void initMarginSpinner() {
+
+        mMarginsSpinner = findViewById(R.id.marginsSpinner);
+
+        ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(
+                this,
+                R.array.margins,
+                android.R.layout.simple_spinner_item
+        );
+        adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mMarginsSpinner.setAdapter(adapterColor);
+
+    }
+
+    private void setLocaleAndColor(String localePrefix) {
+        Locale locale = new Locale(localePrefix);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Utils.changeMargins(this, mMarginsSpinner.getSelectedItemPosition());
+        //recreate();
+    }
+
+
 }
--- app/src/main/java/com/example/androidpanin/Utils.java
+++ app/src/main/java/com/example/androidpanin/Utils.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 public class Utils
 {
     private static int sTheme;
+    private static int sMargins;
 
     public final static int THEME_BLACK = 0;
     public final static int THEME_BLUE = 1;
     public final static int THEME_GREEN = 2;
+
+    public final static int MATGIN_SMALL = 0;
+    public final static int MATGIN_MEDIUM = 1;
+    public final static int MATGIN_LARGE = 2;
 
     /**
      * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
@@ -17,6 +22,15 @@ public class Utils
     public static void changeToTheme(Activity activity, int theme)
     {
         sTheme = theme;
+        activity.finish();
+
+        activity.startActivity(new Intent(activity, activity.getClass()));
+
+    }
+
+    public static void changeMargins(Activity activity, int margins)
+    {
+        sMargins = margins;
         activity.finish();
 
         activity.startActivity(new Intent(activity, activity.getClass()));
@@ -37,6 +51,24 @@ public class Utils
                 break;
             case THEME_GREEN:
                 activity.setTheme(R.style.AppThemeGreen);
+                break;
+        }
+    }
+
+    public static void onActivityCreateSetMargins(Activity activity)
+    {
+
+        switch (sMargins)
+        {
+            default:
+            case MATGIN_SMALL:
+                activity.setTheme(R.style.MarginsSmall);
+                break;
+            case MATGIN_MEDIUM:
+                activity.setTheme(R.style.MarginsMedium);
+                break;
+            case MATGIN_LARGE:
+                activity.setTheme(R.style.MarginsLarge);
                 break;
         }
     }
