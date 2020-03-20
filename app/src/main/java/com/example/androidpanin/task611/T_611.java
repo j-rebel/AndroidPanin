48d0ca752340cfc7a324d46601f3930137912aed
Nick Panin <panin@adwz.ru>
2020-03-20 23:17:27 +0300

    excess imports in 611 removed

--- app/src/main/java/com/example/androidpanin/task611/T_611.java
+++ app/src/main/java/com/example/androidpanin/task611/T_611.java
@@ -1,8 +1,6 @@
 package com.example.androidpanin.task611;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
