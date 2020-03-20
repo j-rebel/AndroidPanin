8f26081f559daceac1722b505d6dfd87e972c21a
Nick Panin <panin@adwz.ru>
2020-03-20 23:16:48 +0300

    611 & 612 done

--- app/src/main/java/com/example/androidpanin/task611/T_611.java
+++ app/src/main/java/com/example/androidpanin/task611/T_611.java
@@ -1,9 +1,14 @@
 package com.example.androidpanin.task611;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 import com.example.androidpanin.R;
 import com.example.androidpanin.ToolbarActivity;
@@ -11,16 +16,120 @@ import com.example.androidpanin.Utils;
 
 public class T_611 extends ToolbarActivity {
 
+    private TextView mLogView;
+    private static String logStr = "Activity lifecycle";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String bundleState = (savedInstanceState == null) ? "onCreate::Bundle == null" : "onCreate::Bundle != null";
+        Log.i("T_611", bundleState);
         super.onCreate(savedInstanceState);
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_611);
         initView();
+        mLogView.append("\n" + bundleState);
     }
 
     private void initView() {
+        Log.i("T_611", "initView");
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        mLogView = findViewById(R.id.logView);
+        mLogView.setText(logStr);
+        mLogView.append("\n" + "initView");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("T_611", "onStart");
+        mLogView.append("\n" + "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("T_611", "onResume");
+        mLogView.append("\n" + "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("T_611", "onPause");
+        mLogView.append("\n" + "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("T_611", "onStop");
+        mLogView.append("\n" + "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("T_611", "onDestroy");
+        mLogView.append("\n" + "onDestroy");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("T_611", "onRestart");
+        mLogView.append("\n" + "onRestart");
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Log.i("T_611", "onPostCreate");
+        mLogView.append("\n" + "onPostCreate");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.i("T_611", "onPostResume");
+        mLogView.append("\n" + "onPostResume");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i("T_611", "onKeyDown");
+        mLogView.append("\n" + "onKeyDown");
+        //return super.onKeyDown(keyCode, event);
+        return true;
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        Log.i("T_611", "onKeyLongPress");
+        mLogView.append("\n" + "onKeyLongPress");
+        return super.onKeyLongPress(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.i("T_611", "onBackPressed");
+        mLogView.append("\n" + "onBackPressed");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i("T_611", "onSaveInstanceState");
+        mLogView.append("\n" + "onSaveInstanceState");
+        logStr = mLogView.getText().toString();
+        super.onSaveInstanceState(outState);
+    }
+
+    /*@Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i("T_611", "onRestoreInstanceState");
+        mLogView.append("\n" + "onRestoreInstanceState");
+    }*/
 }
--- app/src/main/java/com/example/androidpanin/task612/T_612.java
+++ app/src/main/java/com/example/androidpanin/task612/T_612.java
@@ -1,26 +1,128 @@
 package com.example.androidpanin.task612;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.androidpanin.R;
 import com.example.androidpanin.ToolbarActivity;
 import com.example.androidpanin.Utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class T_612 extends ToolbarActivity {
+
+    private List<Map<String, String>> mapList = new ArrayList<>();
+    private String[] strings;
+    private BaseAdapter listContentAdapter;
+    private SwipeRefreshLayout swipeLayout;
+    private ArrayList<Integer> removedInd = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_612);
+        if(savedInstanceState != null) {
+            removedInd = savedInstanceState.getIntegerArrayList("Removed Ids");
+        }
         initView();
     }
 
     private void initView() {
+
+        initToolbar();
+        ListView list = findViewById(R.id.list);
+        String resString = getString(R.string.large_text);
+
+        SharedPreferences sp = getSharedPreferences("T_612", MODE_PRIVATE);
+
+        if (sp.getBoolean("firstRun", true)) {
+            String LARGE_TEXT = "default text";
+            sp.edit().putString(LARGE_TEXT, resString).putBoolean("firstRun", false).apply();
+        }
+
+        strings = sp.getString("default text", "").split("\n");
+
+        fillMapList();
+        removeIndxsFromMapList();
+
+        String[] from = {"Заголовок", "Подзаголовок"};
+        int[] to = {R.id.bookName, R.id.bookDescription};
+
+        listContentAdapter = new SimpleAdapter(
+                this,
+                mapList,
+                R.layout.two_fields_list_item,
+                from,
+                to);
+
+        list.setAdapter(listContentAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mapList.remove(position);
+                removedInd.add(position);
+                listContentAdapter.notifyDataSetChanged();
+            }
+        });
+
+        initSwipe();
+    }
+
+    private void initSwipe() {
+        swipeLayout = findViewById(R.id.swipeRefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                                             @Override
+                                             public void onRefresh() {
+                                                 swipeLayout.setRefreshing(false);
+                                                 mapList.clear();
+                                                 fillMapList();
+                                                 removedInd.clear();
+                                                 listContentAdapter.notifyDataSetChanged();
+                                             }
+                                         }
+        );
+    }
+
+    private void initToolbar() {
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
+
+    private void fillMapList() {
+        for (String string : strings) {
+            Map<String, String> map = new HashMap<>();
+            map.put("Заголовок", string);
+            map.put("Подзаголовок", String.valueOf(string.length()));
+            mapList.add(map);
+        }
+    }
+
+    private void removeIndxsFromMapList() {
+        if(removedInd != null) {
+            for (Integer i : removedInd) {
+                mapList.remove(i.intValue());
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putIntegerArrayList("Removed Ids", removedInd);
+        super.onSaveInstanceState(outState);
+    }
+
+
 }
